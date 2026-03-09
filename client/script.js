376b18486c0aa116c4657fa50e98de8deb879700
Neetesh Kumar Yadav <neeteshyadav0206@email.com>
2026-03-09 11:00:06 +0530

    ,

--- client/script.js
+++ client/script.js
@@ -1,9 +1,9 @@
-// const socket = io("http://192.168.1.123:5000");
+const socket = io("http://192.168.1.123:5000");
 // const socket = io('https://cw6wsg7n-5000.inc1.devtunnels.ms/')  
-const socket = io();
+// const socket = io();
 
 let localStream;
-let peerConnection;
+let peerConnections={};
 
 const localVideo = document.getElementById("localVideo");
 const remoteVideo = document.getElementById("remoteVideo");
@@ -78,63 +78,88 @@ async function joinRoom() {
   }
 }
 
-function createPeerConnection(roomId) {
-  peerConnection = new RTCPeerConnection(servers);
+// function createPeerConnection(roomId) {
+//   peerConnection = new RTCPeerConnection(servers);
   
-  // Add all tracks from local stream
-  if (localStream) {
-    localStream.getTracks().forEach(track => {
-      peerConnection.addTrack(track, localStream);
-    });
+//   // Add all tracks from local stream
+//   if (localStream) {
+//     localStream.getTracks().forEach(track => {
+//       peerConnection.addTrack(track, localStream);
+//     });
+//   }
+  
+//   peerConnection.ontrack = event => {
+//     if (!remoteVideo.srcObject) {
+//       remoteVideo.srcObject = event.streams[0];
+//     }
+//   };
+  
+//   peerConnection.onicecandidate = event => {
+//     if (event.candidate) {
+//       socket.emit("ice-candidate", {
+//         room: roomId,
+//         candidate: event.candidate
+//       });
+//     }
+//   };
+// }
+function createPeerConnection(socketId){
+  const pc= new RTCPeerConnection(servers);
+  localStream.getTracks().forEach(track => {
+pc.addTrack(track,localStream);    
+  });
+  pc.ontrack=event=>{
+    const video=document.createElement("video");
+    video.srcObject=event.streams[0];
+    video.autoplay=true;
+    video.playsInline=true;
+   document.getElementById("videos").appendChild(video);
+  };
+  pc.onicecandidate=event=>{
+    if(event.candidate){
+      socket.emit("ice-candidate",{
+        to:socketId,
+        candidate:event.candidate
+      })
+    }
   }
-  
-  peerConnection.ontrack = event => {
-    if (!remoteVideo.srcObject) {
-      remoteVideo.srcObject = event.streams[0];
-    }
-  };
-  
-  peerConnection.onicecandidate = event => {
-    if (event.candidate) {
-      socket.emit("ice-candidate", {
-        room: roomId,
-        candidate: event.candidate
-      });
-    }
-  };
+  peerConnections[socketId]=pc;
+  return pc;
 }
 
-socket.on("user-joined", async () => {
-  const offer = await peerConnection.createOffer();
-  await peerConnection.setLocalDescription(offer);
+
+socket.on("user-joined", async (socketId) => {
+  const pc=createPeerConnection(socketId);
+  const offer = await pc.createOffer();
+  await pc.setLocalDescription(offer);
   
   socket.emit("offer", {
-    room: roomInput.value,
+  to:socketId,
     offer: offer
   });
 });
 
-socket.on("offer", async (offer) => {
-  if (!peerConnection) {
-    createPeerConnection(roomInput.value);
-  }
-  
-  await peerConnection.setRemoteDescription(offer);
-  const answer = await peerConnection.createAnswer();
-  await peerConnection.setLocalDescription(answer);
+socket.on("offer", async (data) => {
+const pc=createPeerConnection(data.from);
+  await pc.setRemoteDescription(data.offer);
+  const answer = await pc.createAnswer();
+  await pc.setLocalDescription(answer);
   
   socket.emit("answer", {
-    room: roomInput.value,
+    to:data.from,
     answer: answer
   });
 });
 
-socket.on("answer", async (answer) => {
-  await peerConnection.setRemoteDescription(answer);
+socket.on("answer", async (data) => {
+    const pc = peerConnections[data.from];
+
+  await pc.setRemoteDescription(data.answer);
 });
 
-socket.on("ice-candidate", async (candidate) => {
-  if (peerConnection) {
-    await peerConnection.addIceCandidate(new RTCIceCandidate(candidate));
+socket.on("ice-candidate", async (data) => {
+  const pc = peerConnections[data.from];
+  if (pc) {
+    await pc.addIceCandidate(new RTCIceCandidate(data.candidate));
   }
 });
--- server.js
+++ server.js
@@ -1,46 +1,70 @@
-const express=require('express');
-const http=require('http');
-const {Server}=require('socket.io');
-const cors=require('cors');
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
 
-const app=express();
+const app = express();
 app.use(cors());
 
 const path = require("path");
-
 app.use(express.static(path.join(__dirname, "client")));
 
+const server = http.createServer(app);
 
-const server=http.createServer(app);
-
-const io=new Server(server,{
-    cors:{
-        origin:"*",
-    }
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  }
 });
 
-io.on("connection",(socket)=>{
-    console.log("User connected",socket.id);
-socket.on("join-room",(roomId)=>{
+io.on("connection", (socket) => {
+
+  console.log("User connected:", socket.id);
+
+  socket.on("join-room", (roomId) => {
+
     socket.join(roomId);
-    socket.to(roomId).emit("user-joined");
-});
-socket.on("offer",(data)=>{
-    socket.to(data.room).emit("offer",data.offer);
-});
-socket.on("answer",(data)=>{
-    socket.to(data.room).emit("answer",data.answer);
-});
-socket.on("ice-candidate",(data)=>{
-    socket.to(data.room).emit("ice-candidate",data.candidate);
+
+    // Notify others
+    socket.to(roomId).emit("user-joined", socket.id);
+
+    // Send existing users to new user
+    const clients = [...(io.sockets.adapter.rooms.get(roomId) || [])];
+
+    socket.emit("all-users", clients.filter(id => id !== socket.id));
+  });
+
+  socket.on("offer", (data) => {
+
+    socket.to(data.to).emit("offer", {
+      offer: data.offer,
+      from: socket.id
+    });
+
+  });
+
+  socket.on("answer", (data) => {
+
+    socket.to(data.to).emit("answer", {
+      answer: data.answer,
+      from: socket.id
+    });
+
+  });
+
+  socket.on("ice-candidate", (data) => {
+
+    socket.to(data.to).emit("ice-candidate", {
+      candidate: data.candidate,
+      from: socket.id
+    });
+
+  });
+
 });
 
-
-
-
-
-});
 const PORT = process.env.PORT || 5000;
-server.listen(PORT,()=>{
-  console.log("server os running on port 50000");
+
+server.listen(PORT, () => {
+  console.log("Server running on port 5000");
 });
